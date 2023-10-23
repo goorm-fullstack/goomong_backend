d50ed79bc27ecd0a42bb973142858f2ef75045db
LEE-Donggyu <ehdrb141@naver.com>
2023-10-23 15:15:32 +0900

    BE 환경 구축

---
+++ src/main/java/R/VD/goomong/GoomongApplication.java
@@ -0,0 +1,13 @@
+package R.VD.goomong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoomongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoomongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/R/VD/goomong/GoomongApplicationTests.java
@@ -0,0 +1,13 @@
+package R.VD.goomong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoomongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
