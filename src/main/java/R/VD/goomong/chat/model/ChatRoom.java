992441c4485eeae5997af3ced409da4f504caa8d
jeosong <13bitadc@naver.com>
2023-10-27 21:05:13 +0900

    refactor: change chat

--- src/main/java/R/VD/goomong/chat/model/ChatRoom.java
+++ src/main/java/R/VD/goomong/chat/model/ChatRoom.java
@@ -1,5 +1,6 @@
 package R.VD.goomong.chat.model;
 
+import R.VD.goomong.item.model.Item;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,6 +22,10 @@ public class ChatRoom {
 
     @Column(name = "room_name", nullable = false)
     private String roomName;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = true)
+    private Item item;
 
     @OneToMany(mappedBy = "chatRoom")
     private List<ChatMessage> messages;
