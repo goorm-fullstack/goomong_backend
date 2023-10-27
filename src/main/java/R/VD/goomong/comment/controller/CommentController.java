5becc60ddd9090b6caa4ebd9592f6d3fa57082b7
jinhwan Bea <paewhal20@gmail.com>
2023-10-27 14:42:44 +0900

    Update CommentController.java

--- src/main/java/R/VD/goomong/comment/controller/CommentController.java
+++ src/main/java/R/VD/goomong/comment/controller/CommentController.java
@@ -36,8 +36,8 @@ public class CommentController {
     /**
      * 댓글 작성
      *
-     * @param requestCommentDto - 댓글 작성 request
-     * @return - 작성 완료 시 200
+     * @param requestCommentDto 댓글 작성 request
+     * @return 작성 완료 시 200
      */
     @PostMapping("/comment")
     public ResponseEntity<Object> initComment(@Validated @RequestBody RequestCommentDto requestCommentDto) {
@@ -49,9 +49,9 @@ public class CommentController {
     /**
      * 댓글 수정
      *
-     * @param commentId         - 수정할 댓글 pk
-     * @param requestCommentDto - 댓글 수정 내용
-     * @return - 수정된 댓글
+     * @param commentId         수정할 댓글 pk
+     * @param requestCommentDto 댓글 수정 내용
+     * @return 수정된 댓글
      */
     @PutMapping("/comment/{commentId}")
     public ResponseEntity<ResponseCommentDto> updateComment(@PathVariable Long commentId, @Validated @RequestBody RequestCommentDto requestCommentDto) {
@@ -64,8 +64,8 @@ public class CommentController {
     /**
      * 댓글 소프트딜리트
      *
-     * @param commentId - 삭제할 댓글 pk
-     * @return - 삭제 완료 시 200
+     * @param commentId 삭제할 댓글 pk
+     * @return 삭제 완료 시 200
      */
     @PutMapping("/comment/softdel/{commentId}")
     public ResponseEntity<Object> softDeleteComment(@PathVariable Long commentId) {
@@ -77,8 +77,8 @@ public class CommentController {
     /**
      * 댓글 완전삭제
      *
-     * @param commentId - 삭제할 댓글 pk
-     * @return - 삭제 완료 시 200
+     * @param commentId 삭제할 댓글 pk
+     * @return 삭제 완료 시 200
      */
     @DeleteMapping("/comment/{commentId}")
     public ResponseEntity<Object> deleteComment(@PathVariable Long commentId) {
@@ -90,8 +90,8 @@ public class CommentController {
     /**
      * 삭제된 댓글 복구
      *
-     * @param commentId - 복구할 댓글 pk
-     * @return - 복구 완료 시 200
+     * @param commentId 복구할 댓글 pk
+     * @return 복구 완료 시 200
      */
     @PutMapping("/comment/undel/{commentId}")
     public ResponseEntity<Object> unDeleteComment(@PathVariable Long commentId) {
@@ -103,8 +103,8 @@ public class CommentController {
     /**
      * 특정 댓글 조회
      *
-     * @param commentId - 조회할 댓글 pk
-     * @return - 조회된 댓글
+     * @param commentId 조회할 댓글 pk
+     * @return 조회된 댓글
      */
     @GetMapping("/comment/{commentId}")
     public ResponseEntity<ResponseCommentDto> viewComment(@PathVariable Long commentId) {
@@ -116,8 +116,8 @@ public class CommentController {
     /**
      * 댓글 좋아요 클릭
      *
-     * @param commentId - 좋아요 클릭할 댓글 pk
-     * @return - 댓글의 좋아요 수
+     * @param commentId 좋아요 클릭할 댓글 pk
+     * @return 댓글의 좋아요 수
      */
     @GetMapping("/comment/like/{commentId}")
     public ResponseEntity<Integer> increaseLike(@PathVariable Long commentId) {
@@ -130,8 +130,8 @@ public class CommentController {
     /**
      * 삭제되지 않은 댓글 조회
      *
-     * @param pageable - 페이징
-     * @return - 조회된 댓글
+     * @param pageable 페이징
+     * @return 조회된 댓글
      */
     @GetMapping
     @CrossOrigin(exposedHeaders = {"TotalPages", "TotalData"})
@@ -143,8 +143,8 @@ public class CommentController {
     /**
      * 삭제된 댓글 조회
      *
-     * @param pageable - 페이징
-     * @return - 조회된 댓글
+     * @param pageable 페이징
+     * @return 조회된 댓글
      */
     @GetMapping("/deleted")
     @CrossOrigin(exposedHeaders = {"TotalPages", "TotalData"})
@@ -156,8 +156,8 @@ public class CommentController {
     /**
      * 전체 댓글 리스트(삭제된 것과 삭제 안된 것 모두 포함)
      *
-     * @param pageable - 페이징
-     * @return - 조회된 댓글
+     * @param pageable 페이징
+     * @return 조회된 댓글
      */
     @GetMapping("/all")
     @CrossOrigin(exposedHeaders = {"TotalPages", "TotalData"})
