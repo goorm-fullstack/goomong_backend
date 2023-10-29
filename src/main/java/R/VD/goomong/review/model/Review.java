d23d4ed9fdf4f4f4a946c5435393467df07b1a7c
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2023-10-30 01:03:31 +0900

    리뷰 구현 중

--- src/main/java/R/VD/goomong/review/model/Review.java
+++ src/main/java/R/VD/goomong/review/model/Review.java
@@ -1,47 +1,50 @@
 package R.VD.goomong.review.model;
 
+import R.VD.goomong.global.model.BaseDateEntity;
 import R.VD.goomong.image.model.Image;
 import R.VD.goomong.item.model.Item;
 import R.VD.goomong.member.model.Member;
+import R.VD.goomong.review.dto.response.ResponseReviewDto;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@Builder
-public class Review {
+@Builder(toBuilder = true)
+public class Review extends BaseDateEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;//DB 인덱스
 
-    @ManyToOne
-    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
     private Member member;//작성자
 
-    @ManyToOne
-    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
     private Item item;//리뷰를 남길 아이템
 
     @OneToMany
+    @JoinColumn(name = "review_id")
+    @Builder.Default
     private List<Image> imageList = new ArrayList<>();
 
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
     private String content;//리뷰 내용
+
+    @Column(nullable = false)
     private Float rate;//평점
 
-    public void setItem(Item item) {
-        this.item = item;
-    }
-
-    public void setImageList(List<Image> imageList) {
-        this.imageList = imageList;
+    public ResponseReviewDto toResponseReviewDto(){
+        return ResponseReviewDto
     }
 }
