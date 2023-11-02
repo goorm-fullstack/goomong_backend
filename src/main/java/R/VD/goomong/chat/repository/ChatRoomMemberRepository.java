f31c52e520d42907720cbf08ed6ea14720451949
jeosong <13bitadc@naver.com>
2023-11-02 15:41:29 +0900

    Merge develop into feature-jeosong and resolve conflicts

--- src/main/java/R/VD/goomong/chat/repository/ChatRoomMemberRepository.java
+++ src/main/java/R/VD/goomong/chat/repository/ChatRoomMemberRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ChatRoomMemberRepository extends JpaRepository<ChatRoomMember, Long> {
-    @Query("SELECT crm, m.name FROM ChatRoomMember crm JOIN crm.member m WHERE crm.member.id != :memberId")
+    @Query("SELECT crm, m.memberName FROM ChatRoomMember crm JOIN crm.member m WHERE crm.member.id != :memberId")
     List<Object[]> findByMemberIdWithJoinFetch(@Param("memberId") Long memberId);
 
     void deleteByChatRoomAndMember(ChatRoom chatRoom, Member member);
