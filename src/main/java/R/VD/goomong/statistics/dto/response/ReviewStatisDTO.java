f7bdcd2efd2bf5ee7c3b7992b390c4621848a12b
jeosong <13bitadc@naver.com>
2023-11-09 18:15:21 +0900

    feat: statistics

--- src/main/java/R/VD/goomong/statistics/dto/response/ReviewStatisDTO.java
+++ src/main/java/R/VD/goomong/statistics/dto/response/ReviewStatisDTO.java
@@ -24,11 +24,11 @@ public class ReviewStatisDTO {
 
     public ReviewStatisDTO setCustomerSatisfaction(Double avgRating) {
         BigDecimal satisfaction = new BigDecimal(avgRating)
-                .divide(new BigDecimal(5.0), 2, RoundingMode.HALF_UP) // 5.0으로 나누고, 소수점 두 자리에서 반올림합니다.
-                .multiply(new BigDecimal(100)) // 100을 곱하여 퍼센트로 변환합니다.
-                .setScale(1, RoundingMode.HALF_UP); // 소수점 첫째 자리에서 반올림하여 결과를 설정합니다.
+                .divide(new BigDecimal(5.0), 2, RoundingMode.HALF_UP)
+                .multiply(new BigDecimal(100))
+                .setScale(1, RoundingMode.HALF_UP);
 
-        this.customerSatisfaction = satisfaction.doubleValue(); // 계산된 값으로 고객 만족도를 설정합니다.
+        this.customerSatisfaction = satisfaction.doubleValue();
         return this;
     }
 
