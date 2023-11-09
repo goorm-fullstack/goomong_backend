66ab4bf15faf120596d8aae52b515bc2d78e4f4e
jeosong <13bitadc@naver.com>
2023-11-09 11:43:17 +0900

    feat: add ranking

--- src/main/java/R/VD/goomong/ranking/controller/RankingController.java
+++ src/main/java/R/VD/goomong/ranking/controller/RankingController.java
@@ -7,8 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,8 +21,9 @@ public class RankingController {
     private final RankingService rankingService;
 
     @GetMapping("/{period}")
-    public ResponseEntity<List<ResponseRanking>> getRanking(@RequestParam RankingPeriod period) {
-        List<ResponseRanking> ranking = rankingService.getRanking(period);
+    public ResponseEntity<List<ResponseRanking>> getRanking(@PathVariable String period) {
+        RankingPeriod rankingPeriod = RankingPeriod.valueOf(period.toUpperCase());
+        List<ResponseRanking> ranking = rankingService.getRanking(rankingPeriod);
         return new ResponseEntity<>(ranking, HttpStatus.OK);
     }
 
