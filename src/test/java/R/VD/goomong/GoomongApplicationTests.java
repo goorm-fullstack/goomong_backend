a675e5a1e9de05ec4dfff794f99d952962a3bf08
jeosong <13bitadc@naver.com>
2023-11-15 12:17:57 +0900

    배포를 위한 변경

--- src/test/java/R/VD/goomong/GoomongApplicationTests.java
+++ src/test/java/R/VD/goomong/GoomongApplicationTests.java
@@ -1,13 +1,13 @@
-package R.VD.goomong;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class GoomongApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package R.VD.goomong;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class GoomongApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
