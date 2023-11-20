eae1fdfd7b7c1a58510652a414fedab8d5daa5c3
jeosong <13bitadc@naver.com>
2023-11-20 21:08:06 +0900

    배포를 위한 세팅

--- src/main/java/R/VD/goomong/global/config/WebCorsConfig.java
+++ src/main/java/R/VD/goomong/global/config/WebCorsConfig.java
@@ -11,7 +11,7 @@ public class WebCorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://localhost:3000")
+                .allowedOrigins("https://k5fb919a75e08a.user-app.krampoline.com/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
