16190f5ad13be601c538aef7786d2cd6654807cb
jeosong <13bitadc@naver.com>
2023-11-21 12:26:08 +0900

    배포 테스트 환경

--- src/main/java/R/VD/goomong/chat/config/WebSockConfig.java
+++ src/main/java/R/VD/goomong/chat/config/WebSockConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/chat")
+        registry.addEndpoint("/wss/chat")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
