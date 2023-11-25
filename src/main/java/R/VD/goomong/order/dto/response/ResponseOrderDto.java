88f234dd06b2cd8cf6bffeef1cdbd918cc7b5e68
WhiteKIM <9rudrb40@naver.com>
2023-11-25 14:49:47 +0900

    2023.11.25 upload

--- src/main/java/R/VD/goomong/order/dto/response/ResponseOrderDto.java
+++ src/main/java/R/VD/goomong/order/dto/response/ResponseOrderDto.java
@@ -1,31 +1,35 @@
 package R.VD.goomong.order.dto.response;
 
 import R.VD.goomong.global.model.Address;
-import R.VD.goomong.item.model.Item;
-import R.VD.goomong.member.model.Member;
+import R.VD.goomong.item.dto.response.ResponseItemDto;
+import R.VD.goomong.member.dto.response.ResponseMember;
 import R.VD.goomong.order.model.Order;
 import R.VD.goomong.order.model.Status;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class ResponseOrderDto {
     private Long id;
-    private Item orderItem;
-    private Member member;
+    private ResponseItemDto orderItem;
+    private ResponseMember member;
     private int price;
     private int point;
     private Address address;
     private Status status;//배송 상태
     private String orderNumber;
+    private LocalDateTime regDate;
 
     public ResponseOrderDto(Order order) {
         this.id = order.getId();
-        this.orderItem = order.getOrderItem();
-        this.member = order.getMember();
+        this.orderItem = new ResponseItemDto(order.getOrderItem());
+        this.member = new ResponseMember(order.getMember());
         this.price = order.getPrice();
         this.point = order.getPoint();
         this.address = order.getAddress();
         this.status = order.getStatus();
         this.orderNumber = order.getOrderNumber();
+        this.regDate = order.getRegDate();
     }
 }
